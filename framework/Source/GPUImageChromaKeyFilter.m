44639b0542fb41e8e1afe8b3122a02b2ce7f6da0
Kunihiko Karasawa <karasawa@unda.me>
2016-09-05 17:27:59 +0900

    Fixed chromakey filter

--- framework/Source/GPUImageChromaKeyFilter.m
+++ framework/Source/GPUImageChromaKeyFilter.m
@@ -17,20 +17,21 @@ NSString *const kGPUImageChromaKeyFragmentShaderString = SHADER_STRING
  
  void main()
  {
-     vec4 textureColor = texture2D(inputImageTexture, textureCoordinate);
-     
-     float maskY = 0.2989 * colorToReplace.r + 0.5866 * colorToReplace.g + 0.1145 * colorToReplace.b;
-     float maskCr = 0.7132 * (colorToReplace.r - maskY);
-     float maskCb = 0.5647 * (colorToReplace.b - maskY);
-     
-     float Y = 0.2989 * textureColor.r + 0.5866 * textureColor.g + 0.1145 * textureColor.b;
-     float Cr = 0.7132 * (textureColor.r - Y);
-     float Cb = 0.5647 * (textureColor.b - Y);
-     
-     //     float blendValue = 1.0 - smoothstep(thresholdSensitivity - smoothing, thresholdSensitivity , abs(Cr - maskCr) + abs(Cb - maskCb));
-     float blendValue = smoothstep(thresholdSensitivity, thresholdSensitivity + smoothing, distance(vec2(Cr, Cb), vec2(maskCr, maskCb)));
-//     gl_FragColor = vec4(textureColor.rgb, textureColor.a * blendValue);
-	 gl_FragColor = vec4(textureColor.rgb * blendValue, 1.0 * blendValue);
+	 vec4 textureColor = texture2D(inputImageTexture, textureCoordinate);
+	 
+	 float maskY = 0.2989 * colorToReplace.r + 0.5866 * colorToReplace.g + 0.1145 * colorToReplace.b;
+	 float maskCr = 0.7132 * (colorToReplace.r - maskY);
+	 float maskCb = 0.5647 * (colorToReplace.b - maskY);
+	 
+	 float Y = 0.2989 * textureColor.r + 0.5866 * textureColor.g + 0.1145 * textureColor.b;
+	 float Cr = 0.7132 * (textureColor.r - Y);
+	 float Cb = 0.5647 * (textureColor.b - Y);
+	 
+	 //     float blendValue = 1.0 - smoothstep(thresholdSensitivity - smoothing, thresholdSensitivity , abs(Cr - maskCr) + abs(Cb - maskCb));
+	 float blendValue = smoothstep(thresholdSensitivity, thresholdSensitivity + smoothing, distance(vec2(Cr, Cb), vec2(maskCr, maskCb)));
+     gl_FragColor = vec4(textureColor.rgb, textureColor.a * blendValue);
+//	 gl_FragColor = vec4(textureColor.rgb, blendValue);
+//	 gl_FragColor = vec4(textureColor.rgb * blendValue, 1.0 * blendValue);
  }
 );
 #else
